98208bba20ee475fc4debdc84ca3dbfdc7b97584
Alvis <alvishay02@gmail.com>
2023-08-25 21:22:07 +1000

    fixed up navigation issues

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import './css/style.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Using Routes instead of Switch
+import { Route, Routes } from 'react-router-dom'; // Fixed the import here
 import Navbar from './components/navbar/navbar.js';
 import Home from './components/home/home.js';
 import Assets from './components/asset/assets.js';
 
 function App() {
   return (
-    <Router>
         <div className="App">
             <Navbar />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/assets" element={<Assets />} />
             </Routes>
         </div>
-    </Router>
->>>>>>> Stashed changes
   );
 }
 
