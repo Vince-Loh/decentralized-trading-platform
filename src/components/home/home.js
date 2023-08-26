05e4456fd699d23c7abe0205dd175414023cbe0b
Alvis <alvishay02@gmail.com>
2023-08-26 18:19:05 +1000

    changed to using grid system

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,21 +1,30 @@
-import "./homestyles.css";
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Button } from '@mui/material';
 import blockchainImage from './blockchain_2.jpg';
+import './homestyles.css';
 
 function Home() {
     return (
-        
-            <div className="home-container">
-                <div className="hero-banner">
-                    <h2>
-                        Welcome To Our Platform<br></br>
-                        <span id="discover">Discover your <span id="ease">assets of interest</span> and trade them with ease</span> <br></br>
-                        <Link to="/assets"><button className="btn btn-primary">Get Started</button></Link>
-                    </h2>
-                    <img src={blockchainImage} alt="Blockchain" width="600" className="imgBlockchain"/>
-                </div>
-            </div>
+        <div className="home-container">
+            <Grid container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={6}>
+                    <div className="hero-banner">
+                        <h2>
+                            Welcome To Our Platform<br />
+                            <span style={{ color: '#222' }}>
+                                Discover your <span style={{ color: '#2B86C5' }}>assets of interest</span> and trade them with ease
+                            </span>
+                        </h2>
+                        <Link to="/assets" style={{ textDecoration: 'none'}}>
+                            <Button variant="contained" color="primary">
+                                Get Started
+                            </Button>
+                        </Link>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
     );
 }
 
