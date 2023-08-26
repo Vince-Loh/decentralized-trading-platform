3dc71dba0db9b7d07330456a0fa72fa620a23cc8
Alvis <alvishay02@gmail.com>
2023-08-27 01:17:59 +1000

    commit footer component and homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'; // Fixed the import here
 import Navbar from './components/navbar/navbar.js';
 import Home from './components/home/home.js';
 import Assets from './components/asset/assets.js';
+import Footer from './footer/footer.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/assets" element={<Assets />} />
             </Routes>
+            <Footer />
         </div>
   );
 }
